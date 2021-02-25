348c902b2734a3b527d9aacdf5560114e2887ecf
Reinaldo Cardenas <dev.reinaldo.cardenas@gmail.com>
2021-02-25 08:54:21 -0400

    second commmit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,33 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::Table('roles')->insert([
+        'name'=>'super-admin',
+        'slug'=>'vip',
+        'description'=>'highest role',
+        'code'=>'vip-code',
+        ]);
+
+        DB::Table('roles')->insert([
+            'name'=>'admin',
+            'slug'=>'admin',
+            'description'=>'admin role',
+            'code'=>'admin-code',
+        ]);
+
+
+        DB::Table('roles')->insert([
+            'name'=>'distributor',
+            'slug'=>'dist',
+            'description'=>'distributor role',
+            'code'=>'dist-code',
+        ]);
+
+        DB::Table('roles')->insert([
+            'name'=>'hotel',
+            'slug'=>'hotel',
+            'description'=>'hotel role',
+            'code'=>'hotel-code',
+        ]);
     }
 }
